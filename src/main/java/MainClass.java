954f9c8687ad9f8358abcf0989e5fbad5bd41ccf
Richa <richa@tothenew.com>
2018-02-02 20:06:56 +0530

    Added File Type detection Demo Using File Inputstream

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,4 +1,10 @@
 import org.apache.tika.Tika;
+import org.apache.tika.detect.DefaultDetector;
+import org.apache.tika.detect.Detector;
+import org.apache.tika.io.CloseShieldInputStream;
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,9 +17,6 @@ import java.util.stream.Stream;
 public class MainClass {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World");
-
-        System.out.println("Hello World");
         //Add path to your directory
         Path path = Paths.get("/home/richa/Desktop/TestFileType");
         Stream<Path> files = Files.walk(path);
@@ -37,10 +40,18 @@ public class MainClass {
        /* file = new File("/home/richa/Desktop/SM_Docs/.ms_client_keystore");
         System.out.println("FileName:" + file.getName() + "---- Content Type:" + defaultTika.detect(file));*/
 
+
+        final Detector detector = new DefaultDetector();
+        final Metadata metadata = new Metadata();
+
         System.out.println("########################Using Apache Tika- FileInputStream#################################");
         for (File file1 : file.listFiles()) {
             FileInputStream fileInputStream = new FileInputStream(file1);
-            System.out.println("FileName:" + file1.getName() + "---- Content Type:" + defaultTika.detect(fileInputStream));
+            TikaInputStream tikaInputStreamStream = TikaInputStream.get(new CloseShieldInputStream(fileInputStream));
+            metadata.set(Metadata.RESOURCE_NAME_KEY, file1.getName());
+           // System.out.println("FileName:" + file1.getName() + "---- Content Type:" + defaultTika.detect(fileInputStream));4
+           System.out.println("FileName:" + file1.getName() + "---- Content Type:" + detector.detect(tikaInputStreamStream,metadata));
+
         }
 
 
